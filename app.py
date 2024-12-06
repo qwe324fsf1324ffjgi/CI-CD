97ec801e8b056739655ab6c372c303b223f5266c
Your Name <zerahabba1@gmail.com>
2024-12-06 14:14:16 -0800

    commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
